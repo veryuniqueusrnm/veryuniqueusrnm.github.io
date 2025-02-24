92204d945a1f09ced5d04c746bd7328f9efa80c2
veryuniqueusrnm <kiralykevin2007@yahoo.com>
2025-02-24 18:43:21 +0100

    Revert

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,49 @@ $(document).ready(function () {
     }
 });
 
+//Jquery Include HTML
+$(document).ready(function () {
+    function includeHTML() {
+        $('[include-html]').each(function () {
+            var $this = $(this);
+            var file = $this.attr('include-html');
+            if (file) {
+                $.ajax({
+                    url: file,
+                    method: 'GET',
+                    success: function (data) {
+                        $this.html(data);
+                        console.log('Successfully included:', file);
+                    },
+                    error: function () {
+                        console.error('Failed to include:', file);
+                        $this.html('<div class="fallback">Content could not be loaded, using fallback. Please check the console for more information.</div>');
+                    },
+                    complete: function () {
+                        $this.removeAttr('include-html');
+                        includeHTML();
+                    }
+                });
+            }
+        });
+    }
+
+    includeHTML();
+});
+
+$('<style>').prop('type', 'text/css').html(`
+    .fallback {
+        padding: 20px;
+        background-color: #f8d7da;
+        color: #721c24;
+        border: 1px solid #f5c6cb;
+        border-radius: 5px;
+        text-align: center;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        margin: 10px 0;
+    }
+`).appendTo('head');
+
 // Auth0 stuff
 $(document).ready(async function () {
     const accountButton = $('.nav-btn.account');
@@ -241,7 +284,7 @@ $(document).ready(function () {
         const commitUrl = latestCommit.html_url;
 
         $("#commit-info").html(
-            `Latest commit: <strong><a class="link" href="${commitUrl}" target="_blank">${commitId} <i class="fa-solid fa-arrow-up-right-from-square"></i></a></strong> | ${commitDate} - ${commitMessage}. | Brought to you by GitHub.`
+            `Latest commit: <strong><a class="link" href="${commitUrl}" target="_blank">${commitId} <i class="fa-solid fa-arrow-up-right-from-square"></i></a></strong> | ${commitDate}.`
         );
     }).fail(function () {
         console.error("Failed to fetch commit data.");
