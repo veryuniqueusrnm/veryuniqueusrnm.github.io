d196bb1637645452185f1b08b234e09f3f8b96d0
veryuniqueusrnm <kiralykevin2007@yahoo.com>
2025-03-01 04:53:42 -0800

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+// Debugging stuff
+$(document).ready(function() {
+    if (typeof jQuery !== 'undefined') {
+        console.log("jQuery is loaded.");
+        console.log("jQuery version:", jQuery.fn.jquery);
+        if (jQuery.noConflict) {
+            console.log("jQuery is in noConflict mode.");
+        }
+    } else {
+        console.log("jQuery is not loaded.");
+    }
+});
+
 // Get commit info
 $(document).ready(function () {
     const username = "veryuniqueusrnm";
@@ -140,4 +153,43 @@ $(document).ready(function () {
 	};
 
 })(jQuery);
-/* /main promo rotator */
+
+// Script to display device details
+$(document).ready(function () {
+    function getBrowserAndDeviceDetails() {
+        var userAgent = navigator.userAgent;
+        var browserName, deviceType;
+
+        // Detect browser
+        if (userAgent.indexOf("Firefox") > -1) {
+            browserName = "Mozilla Firefox";
+        } else if (userAgent.indexOf("SamsungBrowser") > -1) {
+            browserName = "Samsung Internet";
+        } else if (userAgent.indexOf("Opera") > -1 || userAgent.indexOf("OPR") > -1) {
+            browserName = "Opera";
+        } else if (userAgent.indexOf("Trident") > -1) {
+            browserName = "Microsoft Internet Explorer";
+        } else if (userAgent.indexOf("Edge") > -1) {
+            browserName = "Microsoft Edge";
+        } else if (userAgent.indexOf("Chrome") > -1) {
+            browserName = "Google Chrome";
+        } else if (userAgent.indexOf("Safari") > -1) {
+            browserName = "Apple Safari";
+        } else {
+            browserName = "Unknown Browser";
+        }
+
+        // Detect device
+        if (/Mobi|Android/i.test(userAgent)) {
+            deviceType = "Mobile";
+        } else if (/Tablet|iPad/i.test(userAgent)) {
+            deviceType = "Tablet";
+        } else {
+            deviceType = "Desktop";
+        }
+
+        return `You are using ${browserName} on a ${deviceType} device.`;
+    }
+    
+    $("#browserDetails").text(getBrowserAndDeviceDetails());
+});
