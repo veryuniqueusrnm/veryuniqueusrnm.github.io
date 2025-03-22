3032006f640d16c1b2bb995e130348534e1e15a3
veryuniqueusrnm <kiralykevin2007@yahoo.com>
2025-03-22 19:50:00 +0100

    Update

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+// jQuery menu (mobile)
+$(document).ready(function () {
+    $('.hamburger').click(function () {
+        $(this).toggleClass('active');
+        $('.dropdown').slideToggle(300);
+    });
+
+    $('.dropdown a').not('.menu-button-mobile').click(function () {
+        $('.dropdown').slideUp(300);
+        $('.hamburger').removeClass('active');
+    });
+
+    $('.menu-button-mobile').click(function (e) {
+        e.preventDefault();
+        $(this).next('.submenu').slideToggle(300);
+        $(this).find('i').toggleClass('fa-chevron-down fa-chevron-up');
+    });
+});
+
+// Jquery menu (desktop)
+$(document).ready(function () {
+    $(".menu-container").hover(function () {
+        $(this).find(".menu").stop(true, true).fadeIn(300);
+        $(this).find(".menu-button i").css("transform", "rotate(180deg)");
+    }, function () {
+        $(this).find(".menu").stop(true, true).fadeOut(300);
+        $(this).find(".menu-button i").css("transform", "rotate(0deg)");
+    });
+});
+
+// Function to handle screen width changes
+$(document).ready(function () {
+    function checkScreenWidth() {
+        console.log("CheckScreenWidth");
+    }
+
+    checkScreenWidth();
+
+    $(window).on('resize', checkScreenWidth);
+});
+
+// Get commit info
+$(document).ready(function () {
+    // Change to your Github repository if you plan to use the "latest commit" line in the footer.
+    const username = "veryuniqueusrnm";
+    const repo = "veryuniqueusrnm.github.io";
+    const apiUrl = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    $.getJSON(apiUrl, function (data) {
+        if (!data || data.length === 0) return;
+
+        const latestCommit = data[0];
+        const commitId = latestCommit.sha.substring(0, 7);
+        const commitDate = new Date(latestCommit.commit.author.date).toLocaleDateString();
+        const commitMessage = latestCommit.commit.message;
+        const commitUrl = latestCommit.html_url;
+
+        $("#commit-info").html(
+            `Latest commit: <strong><a class="link" href="${commitUrl}" target="_blank">${commitId} <i class="fa-solid fa-arrow-up-right-from-square"></i></a></strong> | ${commitDate} - ${commitMessage}. | Brought to you by GitHub.`
+        );
+    }).fail(function () {
+        console.error("Failed to fetch commit data.");
+    });
+});
