0965cc3c051a8b29d66c1388a3b772ba9580f3d1
veryuniqueusrnm <kiralykevin2007@yahoo.com>
2025-03-22 17:57:41 +0100

    Add counter

--- assets/js/site.js
+++ assets/js/site.js
@@ -1716,3 +1716,25 @@ $(document).ready(function () {
 
 });
 /* / video player */
+
+var countDownDate = new Date("August 19, 2025 12:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("counter").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = "EXPIRED";
+  }
+}, 1000);
